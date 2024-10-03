c09a65e432968e5b8ebb8fe63262c117fbed7d68
maydamv <moralesvieramayda@gmail.com>
2024-10-03 02:06:09 -0400

    add train models view

---
+++ pages/train-models.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full bg-white flex flex-col rounded-3xl p-4 gap-2">
+    <h1 class="text-custom-primary text-xl font-semibold w-full text-center">
+      Entrenar Modelos
+    </h1>
+    <p class="text-custom-text-200 text-center">
+      {{ `Esta funcionalidad aún esta en desarrollo, vuelva más tarde ;)` }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Entrenar Modelos | Administración",
+});
+
+definePageMeta({
+  middleware: ["auth"],
+  requiresRoles: ["standard-user", "admin"],
+  layout: "dashboard",
+});
+</script>
